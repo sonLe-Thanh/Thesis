e79033f2741163ec02976485070de465c7c0abf4
thuytdv <thuytdv.vn@gmail.com>
2022-05-11 22:14:39 +0700

    France 2

--- plot_Germany.m
+++ plot_Germany.m
@@ -1,18 +1,21 @@
-data_uci = load("Data/Result/France_50_result_uci.csv");
+%data_uci = load("Data/Result/France_50_result_uci.csv");
 data_umass = load("Data/Result/France_50_result_umass.csv");
+data_perplexity = load("Data/Result/France_50_result_perplexity.csv");
 
-plot(data_uci(:,1), data_uci(:,2), "-r", "linewidth",2);
-hold on
+%plot(data_uci(:,1), data_uci(:,2), "-r", "linewidth",2);
+%hold on
 
 plot(data_umass(:,1), data_umass(:,2), "-b", "linewidth",2);
+hold on
 
+plot(data_perplexity(:, 1), data_perplexity(:, 2), "-r", "linewidth", 2);
 
 xlabel("Number of topics", "FontSize", 20);
 ylabel("Topic coherence", "FontSize", 20);
 
 title("Topic coherence with different number of topics", "FontSize", 25);
-legend(["UCI","UMass"], "FontSize", 17);
-
+%legend(["UCI","UMass"], "FontSize", 17);
+legend(["UMass", "Perplexity"], "FontSize", 17);
 hold off 
 
 % 73 for Germany
