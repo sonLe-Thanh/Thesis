6da33232970d1bb4788699592aa28f04925aed50
Le Thanh Son <thanhson@MacBook-Pro-cua-Son.local>
2022-05-11 19:50:29 +0700

    Done Germany

--- plot_Germany.m
+++ plot_Germany.m
@@ -1,22 +1,18 @@
-data_50 = load("Data/Germany_50_result.csv");
+data_uci = load("Data/Result/Germany_50_result_uci.csv");
+data_umass = load("Data/Result/Germany_50_result_umass.csv");
 
-
-plot(data_50(:,1), data_50(:,2), "-r", "linewidth",2);
+plot(data_uci(:,1), data_uci(:,2), "-r", "linewidth",2);
 hold on
+
+plot(data_umass(:,1), data_umass(:,2), "-b", "linewidth",2);
+
+
 xlabel("Number of topics", "FontSize", 20);
 ylabel("Topic coherence", "FontSize", 20);
+
 title("Topic coherence with different number of topics", "FontSize", 25);
-legend("50 iterations", "FontSize", 17);
+legend(["UCI","UMass"], "FontSize", 17);
 
 hold off 
 
-
-% plot(data_50(:,1), data_50(:,3), "-r", "linewidth",2);
-% hold on
-% plot(data_100(:,1), data_100(:,3), "-b", "linewidth",2);
-% xlabel("Number of topics", "FontSize", 20);
-% ylabel("Execution time", "FontSize", 20);
-% title("Execution time with different number of topics", "FontSize", 25);
-% legend(["50 iterations", "100 iterations"], "FontSize", 17);
-% 
-% hold off 
+% 73 for Germany
