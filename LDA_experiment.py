1806dbd32404de893f30b5c86a2c4c57344d6c71
thuytdv <thuytdv.vn@gmail.com>
2022-05-30 04:09:33 +0700

    Smoothed LDA

--- LDA_experiment.py
+++ LDA_experiment.py
@@ -19,7 +19,6 @@ class LdaModel:
         for d, n in enumerate(self.N):
             self.phi[d, n:, :] = 0
         self.gamma = self.alpha + np.ones((self.M, self.K)) * self.N.reshape(-1, 1) / self.K
-        np.set_printoptions(precision=4)
 
     def e_step(self, iterations=50):
         for d in range(self.M):
@@ -86,6 +85,136 @@ class LdaModel:
             lb += (self.phi[d, :self.N[d], :] * (psi(self.gamma[d, :]) - psi(self.gamma[d, :].sum()))).sum(axis=1).sum(axis=0)
 
             lb += (self.phi[d, :self.N[d], :] * np.log(self.beta[:, self.docs[d]].T)).sum(axis=1).sum(axis=0)
+
+            lb -= loggamma(self.gamma[d, :].sum()) - loggamma(self.gamma[d, :]).sum() \
+               + ((self.gamma[d, :] - 1)*(psi(self.gamma[d, :]) - psi(self.gamma[d, :].sum()))).sum()
+
+            lb -= (self.phi[d, :self.N[d], :] * np.log(self.phi[d, :self.N[d], :])).sum(axis=1).sum(axis=0)
+        # return lb / self.M
+        return lb
+
+    def show_topics(self, n=10):
+        """
+        find the index of the largest `n` values in a list
+        """
+        for k in range(self.K):
+            max_values = self.beta[k].argsort()[-n:][::-1]
+            topic_k =  np.array(list(self.vocabulary.keys()))[max_values]
+            print(f"Topic {k:02}: {topic_k}")
+            print(max_values)
+
+
+class SmoothedLdaModel:
+    def __init__(self, docs, num_topics, vocabulary):
+        self.docs = docs # a matrix of size M x N_d
+        self.K = num_topics # a number K
+        self.vocabulary = vocabulary # a list of word in ABC order
+        self.M = len(self.docs)
+        self.N = np.array(list(map(lambda x: len(x), self.docs)))
+        self.V = len(vocabulary)
+
+        # parameter
+        np.random.seed(1)
+        self.alpha = np.random.gamma(shape=100, scale=0.01, size=self.K)
+        # self.beta = np.random.dirichlet(np.ones(self.V), self.K)
+        self.phi = np.ones((self.M, np.max(self.N), self.K)) / self.K # LDA paper
+        for d, n in enumerate(self.N):
+            self.phi[d, n:, :] = 0
+        self.gamma = self.alpha + np.ones((self.M, self.K)) * self.N.reshape(-1, 1) / self.K
+        self._lambda = np.random.gamma(shape=100, scale=0.01, size=(self.K, self.V))
+        self.eta = np.ones(self.V)
+
+    def e_step(self, iterations=50):
+        for d in range(self.M):
+            # update phi
+            self.phi[d, :self.N[d], :] = np.exp(
+                psi(self.gamma[d, :]) - psi(self.gamma[d, :].sum()) \
+                + psi(self._lambda[:, self.docs[d]]).T - psi(self._lambda[:, self.docs[d]].sum(axis=1))
+            )
+
+            # update lambda
+            for i in range(self.V):
+                self._lambda[:, i] = np.array([
+                    (np.array(self.docs[d]) == i) @ self.phi[d, :self.N[d], :] for d in range(self.M)
+                ]).sum(axis=0)
+            self._lambda += self.eta
+
+            # normalize phi
+            self.phi[d, :self.N[d]] /= self.phi[d, :self.N[d]].sum(axis=1).reshape(-1, 1)
+
+        # update gamma
+        self.gamma = self.alpha + self.phi.sum(axis=1)
+
+    def m_step(self):
+        # update alpha and eta: Newton-Raphson
+        self.newton_raphson()
+
+    def newton_raphson(self, max_iter=10000, eps=1e-2):
+        # alpha
+        for _ in range(max_iter):
+            alpha = self.alpha.copy()
+            # gradient
+            g = self.M * (psi(self.alpha.sum()) - psi(self.alpha)) \
+                + (psi(self.gamma) - psi(self.gamma.sum(axis=1)).reshape(-1, 1)).sum(axis=0)
+            # hessian matrix: H = diag(h) + 1z1^T
+            # Compute Newton step: d = -(H^-1g) = -(g-c)/h
+            z = self.M * polygamma(1, self.alpha.sum())
+            h = -self.M * polygamma(1, self.alpha)
+            c = (g / h).sum() / (1./z + (1./h).sum())
+            d = -(g - c) / h
+            self.alpha += d
+
+            # check convergence
+            err = np.linalg.norm(self.alpha - alpha)
+            # print(f"{_}", err)
+            if  err < eps:
+                break
+
+        # eta
+        for _ in range(max_iter):
+            eta = self.eta.copy()
+            # gradient
+            g = self.K * (psi(self.eta.sum()) - psi(self.eta)) \
+                + (psi(self._lambda) - psi(self._lambda.sum(axis=1)).reshape(-1, 1)).sum(axis=0)
+            # hessian matrix: H = diag(h) + 1z1^T
+            # Compute Newton step: d = -(H^-1g) = -(g-c)/h
+            z = self.K * polygamma(1, self.alpha.sum())
+            h = -self.K * polygamma(1, self.alpha)
+            c = (g / h).sum() / (1./z + (1./h).sum())
+            d = -(g - c) / h
+            self.eta += d
+
+            # check convergence
+            err = np.linalg.norm(self.eta - eta)
+            # print(f"{_}", err)
+            if  err < eps:
+                break
+
+    def train(self, max_iter=100, eps=3e-2):
+        lb_old = -np.inf
+        for it in range(max_iter):
+            self.e_step()
+            self.m_step()
+            lb = self.elbo()
+            err = abs(lb - lb_old)
+            lb_old = lb
+            print(f"{it: 04}:  ELBO: {lb: .4f},  error: {err: .3f}")
+            if err < eps:
+                break
+        print("Beta", self.beta)
+
+    def elbo(self):
+        lb = 0
+        for d in range(self.M):
+            lb += self.K*(loggamma(np.sum(self.eta)) - np.sum(loggamma(self.eta))) \
+                + ((self.eta - 1)*(psi(self._lambda) - psi(self._lambda.sum(axis=1)))).sum()
+
+            lb += loggamma(self.alpha.sum()) - loggamma(self.alpha).sum() \
+               + ((self.alpha - 1)*(psi(self.gamma[d, :]) - psi(self.gamma[d, :].sum()))).sum()
+
+            lb += (self.phi[d, :self.N[d], :] * (psi(self.gamma[d, :]) - psi(self.gamma[d, :].sum()))).sum(axis=1).sum(axis=0)
+
+            lb += (self.phi[d, :self.N[d], :] * np.log(self._lambda[:, self.docs[d]].T)).sum(axis=1).sum(axis=0)
 
             lb -= loggamma(self.gamma[d, :].sum()) - loggamma(self.gamma[d, :]).sum() \
                + ((self.gamma[d, :] - 1)*(psi(self.gamma[d, :]) - psi(self.gamma[d, :].sum()))).sum()
